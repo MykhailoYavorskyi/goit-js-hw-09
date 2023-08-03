a585f178676844f5b71c9be53e7040adb7daef51
MykhailoYavorskyi <my70035@gmail.com>
2023-08-03 14:01:48 +0200

    01 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
+refs.stopBtn.disabled = true;
+refs.startBtn.addEventListener('click', onStart);
 
+function onStart() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+
+  const id = setInterval(
+    () => (refs.body.style.backgroundColor = getRandomHexColor()),
+    1000
+  );
+
+  refs.stopBtn.addEventListener('click', onStop);
+  function onStop() {
+    refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
+    clearInterval(id);
+  }
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
