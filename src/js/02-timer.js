fc80c3eff0950edd1708e15f80812f0768cb8506
MykhailoYavorskyi <my70035@gmail.com>
2023-08-03 23:00:36 +0200

    02 start

---
+++ src/js/02-timer.js
@@ -0,0 +1,56 @@
+// Описаний в документації
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.dir(selectedDates[0]);
+  },
+};
+
+const refs = {
+  inputDate: document.querySelector('#datetime-picker'),
+  startBtn: document.querySelector('[data-start]'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+flatpickr(refs.inputDate, options);
+const currentDate = new Date();
+
+refs.startBtn.addEventListener('click', onSetDate);
+
+function onSetDate(e) {
+  const countDown = refs.inputDate._flatpickr.latestSelectedDateObj - currentDate;
+
+  refs.days.textContent = convertMs(countDown).days;
+  refs.hours.textContent = convertMs(countDown).hours;
+  refs.minutes.textContent = convertMs(countDown).minutes;
+  refs.seconds.textContent = convertMs(countDown).seconds;
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
